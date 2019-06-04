00612347292324b4151376539a0e26b037492db7
Paweł Janik <paweljnk@gmail.com>
2019-06-04 20:30:21 +0200

    Simplify accept header

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,3 @@
-const E2E_TESTS_ACCEPT_HEADER = 'application/vnd.kontist.e2e.json';
+const JSON_HEADER = 'application/json';
 
-export const shouldReturnJSON = (req) => [E2E_TESTS_ACCEPT_HEADER].includes(req.headers.accept);
+export const shouldReturnJSON = (req) => req.headers.accept === JSON_HEADER;
