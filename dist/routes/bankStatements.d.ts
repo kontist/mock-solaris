e5d7ffb4a3e9d19229e13c47c36ca45a2d208a22
Johannes Pfeiffer <johannespfeiffer@netnexus.de>
2020-06-19 16:58:06 +0200

    Remove dist from repo

--- dist/routes/bankStatements.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const createBankStatement: (req: any, res: any) => Promise<void>;
-export declare const showBankStatementBookings: (req: any, res: any) => Promise<any>;
--- dist/routes/e2e.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const updatePerson: (req: any, res: any) => Promise<any>;
