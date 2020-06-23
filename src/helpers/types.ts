e995a31d981bc085c725659301d15740ed4d088d
Fabrizio Bettinelli <fabribettinelli@gmail.com>
2020-06-23 08:57:28 -0300

    Remove unrelated type

--- src/helpers/types.ts
+++ src/helpers/types.ts
@@ -204,7 +204,6 @@ export enum CardWebhookEvent {
   "CARD_AUTHORIZATION_RESOLUTION" = "CARD_AUTHORIZATION_RESOLUTION", // 	The reservation was cancelled, expired or booked. No push notification for the customer is required for this webhook.
   "CARD_LIFECYCLE_EVENT" = "CARD_LIFECYCLE_EVENT", // The status of the card is changed.
   "CARD_TOKEN_LIFECYCLE" = "CARD_TOKEN_LIFECYCLE", // Push provisioning token is created or updated.
-  "CARD_FRAUD_CASE" = "CARD_FRAUD_CASE",
 }
 
 export enum OverdraftApplicationWebhookEvent {
