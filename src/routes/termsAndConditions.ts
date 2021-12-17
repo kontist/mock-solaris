2c61739983f15913478d96e447db70889b7e6e65
lera <buhg.odessa@gmail.com>
2021-12-17 13:41:25 +0100

    add errors

--- src/routes/termsAndConditions.ts
+++ src/routes/termsAndConditions.ts
@@ -1,18 +1,59 @@
 import uuid from "node-uuid";
-import { generateEntityNotFoundPayload } from "../helpers/overdraft";
 import { getPerson } from "../db";
+
+const EVENT_TYPES = ["APPROVED", "REJECTED"];
 
 export const createTermsAndConditionsEvent = async (req, res) => {
   const {
-    body: { signed_by: personId },
+    body: {
+      signed_by: personId,
+      event_type: eventType,
+      event_timestamp: eventTimestamp,
+    },
   } = req;
 
   const person = await getPerson(personId);
 
   if (!person) {
-    return res
-      .status(404)
-      .send(generateEntityNotFoundPayload("person_id", personId));
+    return res.status(400).send({
+      id: uuid.v4(),
+      status: 400,
+      code: "validation_error",
+      title: "Validation Error",
+      detail: "signed_by is invalid",
+      source: {
+        message: "is invalid",
+        field: "signed_by",
+      },
+    });
+  }
+
+  if (!eventTimestamp) {
+    return res.status(400).send({
+      id: uuid.v4(),
+      status: 400,
+      code: "validation_error",
+      title: "Validation Error",
+      detail: "event_timestamp is missing",
+      source: {
+        message: "is missing",
+        field: "event_timestamp",
+      },
+    });
+  }
+
+  if (!EVENT_TYPES.includes(eventType)) {
+    return res.status(400).send({
+      id: uuid.v4(),
+      status: 400,
+      code: "validation_error",
+      title: "Validation Error",
+      detail: "event_type does not have a valid value",
+      source: {
+        message: "does not have a valid value",
+        field: "event_type",
+      },
+    });
   }
 
   return res.status(201).send({
