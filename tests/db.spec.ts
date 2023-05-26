cc55f820d94cb6f248fa8ca308e08d3e37f1e162
Uğur Gümüşhan <ugur.gumushan@icloud.com>
2023-05-26 13:04:46 +0300

    isolate getAllPersons test

--- tests/db.spec.ts
+++ tests/db.spec.ts
@@ -3,12 +3,12 @@ import { mockReq, mockRes } from "sinon-express-mock";
 import { getPersons, flushDb } from "../src/db";
 import { createPerson } from "../src/routes/persons";
 
-describe("getAllPersons()", async () => {
+describe("getPersons()", async () => {
   before(flushDb);
   after(flushDb);
 
-  const numPersons = 5;
-  it(`When ${numPersons} persons are added, getAllPersons() returns ${numPersons} persons`, async () => {
+  it(`getAllPersons() returns all persons`, async () => {
+    const numPersons = 5;
     for (let i = 1; i <= numPersons; i++) {
       const req = mockReq({
         body: {
