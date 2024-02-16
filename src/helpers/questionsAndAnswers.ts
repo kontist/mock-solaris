6c753321888c7e8d1685e70f52728b7815f0a1e7
p-janik <paweljnk@gmail.com>
2024-02-16 08:55:44 +0000

    Add timeout

--- src/helpers/questionsAndAnswers.ts
+++ src/helpers/questionsAndAnswers.ts
@@ -4,25 +4,34 @@ import _ from "lodash";
 import generateID from "./id";
 
 const fetchRandomQuestion = async () => {
-  try {
-    const response = await fetch(
-      "https://the-trivia-api.com/v2/questions?limit=1"
-    );
-    const data = await response.json();
-    const answers = _.shuffle([
-      data[0].correctAnswer,
-      ...data[0].incorrectAnswers,
-    ]);
+  const getQuestion = async () => {
+    try {
+      const response = await fetch(
+        "https://the-trivia-api.com/v2/questions?limit=1"
+      );
+      const data = await response.json();
+      const answers = _.shuffle([
+        data[0].correctAnswer,
+        ...data[0].incorrectAnswers,
+      ]);
 
-    // return single line with question and available options, but with number prefix based on comment structure
-    const questionText = `${data[0].question.text}\n${answers
-      .map((answer, index) => `${index + 1}. ${answer}`)
-      .join("\n")}`;
+      // return single line with question and available options, but with number prefix based on comment structure
+      const questionText = `${data[0].question.text}\n${answers
+        .map((answer, index) => `${index + 1}. ${answer}`)
+        .join("\n")}`;
 
-    return questionText;
-  } catch (err) {
-    return "SAMPLE QUESTION";
-  }
+      return questionText;
+    } catch (err) {
+      return "SAMPLE QUESTION";
+    }
+  };
+
+  const results = (await Promise.race([
+    getQuestion(),
+    new Promise((resolve) => setTimeout(resolve, 5000)),
+  ])) as string[];
+
+  return results.filter((r) => r).pop();
 };
 
 export const createQuestionSet = async (personId: string) => {
