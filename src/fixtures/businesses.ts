9556898f1d039c8b3aeed07e5e1d005b774a6b2c
Uğur Gümüşhan <ugur.gumushan@icloud.com>
2024-09-23 11:55:13 +0300

    Add businesses

--- src/fixtures/businesses.ts
+++ src/fixtures/businesses.ts
@@ -24,4 +24,73 @@ export const businesses: Business[] = [
       },
     ],
   },
+  {
+    name: "FLOOR 14 GmbH",
+    address: {
+      country: "DE",
+      postal_code: "86919",
+      city: "Utting a.Ammersee",
+      line_1: "Seestraße 9",
+      line_2: "",
+      state: "Bayern",
+    },
+    legal_form: "GMBH",
+    tax_country: "DE",
+    registration_number: "HRB 198674",
+    registration_issuer: "ISSUER MÜNCHEN",
+    registration_date: "2012-05-09",
+    registry_updated_at: "2015-11-17",
+    legal_representatives: [
+      {
+        first_name: "Deuter",
+        last_name: "Müller",
+      },
+    ],
+  },
+  {
+    name: "COMPANY 15 GmbH",
+    address: {
+      country: "DE",
+      postal_code: "86919",
+      city: "Utting a.Ammersee",
+      line_1: "Seestraße 9",
+      line_2: "",
+      state: "Bavaria",
+    },
+    legal_form: "GMBH",
+    tax_country: "DE",
+    registration_number: "HRB 198674",
+    registration_issuer: "ISSUER BAVARIA",
+    registration_date: "2012-05-10",
+    registry_updated_at: "2015-11-18",
+    legal_representatives: [
+      {
+        first_name: "Hermann",
+        last_name: "Kaltz",
+      },
+    ],
+  },
+  {
+    name: "COMPANY 16 GmbH",
+    address: {
+      country: "DE",
+      postal_code: "86919",
+      city: "Utting a.Ammersee",
+      line_1: "Seestraße 9",
+      line_2: "",
+      state: "Saxony",
+    },
+    legal_form: "GMBH",
+    tax_country: "DE",
+    registration_number: "HRB 198674",
+    registration_issuer: "ISSUER SAXONY",
+    registration_date: "2012-05-10",
+    registry_updated_at: "2015-11-18",
+    legal_representatives: [
+      {
+        first_name: "Cornelius",
+        last_name: "Heine",
+      },
+    ],
+  },
 ];
