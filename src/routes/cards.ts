659b9be86b86578c38a447166fbce25a17284c3c
Sina Hashemy <sina.hashemy@kontist.com>
2025-01-17 17:49:52 +0100

    KRP-1457 Fix issue with creating new card for business account (#238)
    * Fix wrong replacement person.account with business.account
    
    * fix request middleware
    
    * Fix issue on first card
    
    ---------
    
    Co-authored-by: lera <buhg.odessa@gmail.com>

--- src/routes/cards.ts
+++ src/routes/cards.ts
@@ -110,7 +110,10 @@ export const createCardHandler = async (
       person = await db.getPerson(
         business.legalRepresentatives[0].legal_representative_id
       );
-      person.account = business.account;
+
+      if (!person.account) {
+        person.account = business.account;
+      }
     }
 
     if (!person) {
