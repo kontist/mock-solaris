77ed7588d5ff8f144e2d803d99bb838da1b0646c
lera <buhg.odessa@gmail.com>
2025-02-28 17:43:21 +0100

    fix typo

--- src/routes/intraCustomerTransfer.ts
+++ src/routes/intraCustomerTransfer.ts
@@ -128,6 +128,6 @@ export const createIntraCustomerTransfer = async (req, res) => {
     ...transfer,
   });
 
-  await triggerBookingsWebhook(entity, incomingBooking, recipientAccount.is);
+  await triggerBookingsWebhook(entity, incomingBooking, recipientAccount.id);
   await triggerBookingsWebhook(entity, outgoingBooking, senderAccount.id);
 };
