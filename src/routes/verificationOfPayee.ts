54d5d0c0cef6933c6b4c5c6fcc31d020c758e9ac
Mikolaj Kaminski <mikolaj.kaminski789@gmail.com>
2025-08-22 09:53:36 +0200

    feat: improve matching and validation

--- src/routes/verificationOfPayee.ts
+++ src/routes/verificationOfPayee.ts
@@ -3,6 +3,23 @@ import type { Request, Response } from "express";
 import uuid from "node-uuid";
 import HttpStatusCodes from "http-status";
 
+const nameValidation: RegExp = /^[\p{L}\p{M}\p{N}\p{P}\p{S}\s]+$/u;
+
+const noMatchRegex: RegExp = /no[_ ]match/i;
+const notPossibleRegex: RegExp = /not[_ ]possible/i;
+const closeMatchRegex: RegExp = /close[_ ]match/i;
+
+/**
+ * Creates a verification of Payee
+ *
+ * By default will always return a "MATCH" status.
+ * For other statuses use the appropriate regex patterns in the name field.
+ * - "NO_MATCH": if name contains "no match" or "no_match" - case insensitive
+ * - "VERIFICATION_NOT_POSSIBLE": if name contains "not possible" or "not_possible" - case insensitive
+ * - "CLOSE_MATCH": if name contains "close match" or "close_match" - case insensitive
+ *
+ * @see https://docs.solarisgroup.com/api-reference/digital-banking/sepa-transfers/#tag/Verification-of-Payee/paths/~1v1~1verifications_of_payee/post
+ */
 export const verifyPayee = (req: Request, res: Response) => {
   const data = _.pick(req.body, ["iban", "name"]);
   const { iban, name } = data;
@@ -35,29 +52,39 @@ export const verifyPayee = (req: Request, res: Response) => {
     });
   }
 
-  let result;
+  if (!nameValidation.test(name) || name.length > 140) {
+    return res.status(HttpStatusCodes.BAD_REQUEST).send({
+      errors: [
+        {
+          id: uuid.v4(),
+          status: HttpStatusCodes.BAD_REQUEST,
+          code: "bad_request",
+          title: "Bad Request",
+          detail: "Name is not valid.",
+        },
+      ],
+    });
+  }
 
-  switch (name) {
-    case "NO_MATCH":
-      result = {
-        status: "NO_MATCH",
-      };
-      break;
-    case "VERIFICATION_NOT_POSSIBLE":
-      result = {
-        status: "VERIFICATION_NOT_POSSIBLE",
-      };
-      break;
-    case "CLOSE_MATCH":
-      result = {
-        status: "CLOSE_MATCH",
-        suggested_name: "John Doe",
-      };
-      break;
-    default:
-      result = {
-        status: "MATCH",
-      };
+  let result: { status: string; suggested_name?: string } = { status: "MATCH" };
+
+  if (noMatchRegex.test(name)) {
+    result = {
+      status: "NO_MATCH",
+    };
+  }
+
+  if (notPossibleRegex.test(name)) {
+    result = {
+      status: "VERIFICATION_NOT_POSSIBLE",
+    };
+  }
+
+  if (closeMatchRegex.test(name)) {
+    result = {
+      status: "CLOSE_MATCH",
+      suggested_name: "Giovanni Kontistini",
+    };
   }
 
   const dateNow = new Date();
