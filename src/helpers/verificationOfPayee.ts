25202801a9fdd111ac4ee7ac543f4ba5cd25ab42
Mikolaj Kaminski <mikolaj.kaminski789@gmail.com>
2025-08-22 14:14:50 +0200

    feat: push release a few days early for internal testing

--- src/helpers/verificationOfPayee.ts
+++ src/helpers/verificationOfPayee.ts
@@ -1,4 +1,6 @@
-const vopStartDate = new Date("2025-10-09");
+// Date is set to 6th October for internal testing
+// The real final start date is 9th October 2025
+const vopStartDate = new Date("2025-10-06");
 
 export const isVerificationOfPayeeRequired = (): boolean => {
   const currentDate = new Date();
